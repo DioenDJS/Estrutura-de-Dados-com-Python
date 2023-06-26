aecc0fb0bc0dc6b8c92a02360ed2e3913f352f36
Diovane Pacheti <negociospacheti@gmail.com>
2023-06-26 13:06:08 -0300

    :tada: Init

---
+++ node.py
@@ -0,0 +1,6 @@
+class Node:
+
+    #construtor da class com o parametro data que vai ser usado para criar nosso objeto
+    def __init__(self, data):
+        self.data = data  # Atributo do dado
+        self.next = None  # Atributo que armazenara a referencia do proximo atributo
